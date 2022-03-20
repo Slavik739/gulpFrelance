db2ac733447aac56ead8c8173dd7f5379b66c9a8
Stanislav <Svstason@gmail.com>
2022-03-20 13:25:43 +0200

    add fuction 'reset'

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,24 @@
 // основной модуль
 import gulp from 'gulp'
 // импорт путей
-import {path} from './gulp/config/path.js'
+import { path } from './gulp/config/path.js'
 //импорт задач
-import {copy} from './gulp/tasks/copy.js';
+import { copy } from './gulp/tasks/copy.js';
+import { reset } from "./gulp/tasks/reset.js";
 
 //передаем значения в глобальную переменную
 global.app = {
     path: path,
     gulp: gulp
 }
+
 //наблюдать за изменениями в файлах
 function watcher() {
     gulp.watch(path.watch.files, copy);
 }
+
 //построение сценариев выполнения задач
-const dev = gulp.series(copy, watcher);
+const dev = gulp.series(reset, copy, watcher);
 
 // выполнение сценария по умолчанию
 gulp.task('default', dev);
